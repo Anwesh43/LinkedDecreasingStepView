98c5926d932081c2e0de7eb33a47fd7495c7bcc4
Anwesh43 <anweshthecool0@gmail.com>
2018-07-08 10:44:19 +0530

    adding LinkedDecreasingStage class

--- linkeddecreasingstepview/src/main/java/com/anwesh/uiprojects/linkeddecreasingstepview/LinkedDecreasingStage.kt
+++ linkeddecreasingstepview/src/main/java/com/anwesh/uiprojects/linkeddecreasingstepview/LinkedDecreasingStage.kt
@@ -3,3 +3,30 @@ package com.anwesh.uiprojects.linkeddecreasingstepview
 /**
  * Created by anweshmishra on 08/07/18.
  */
+
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.view.View
+import android.view.MotionEvent
+
+val NODES : Int = 5
+
+class LinkedDecreasingStage (ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                
+            }
+        }
+        return true
+    }
+
+}
