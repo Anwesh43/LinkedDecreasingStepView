ce23bd4850aef88840898e54437356eda5c52c09
Anwesh43 <anweshthecool0@gmail.com>
2018-07-08 12:03:24 +0530

    adding CompletionListener in View

--- app/src/main/java/com/anwesh/uiprojects/kotlinlinkeddecreasingstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlinlinkeddecreasingstepview/MainActivity.kt
@@ -3,14 +3,18 @@ package com.anwesh.uiprojects.kotlinlinkeddecreasingstepview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.anwesh.uiprojects.linkeddecreasingstepview.LinkedDecreasingStage
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LinkedDecreasingStage.create(this)
+        val view : LinkedDecreasingStage =  LinkedDecreasingStage.create(this)
         fullScreen()
+        view.addOnCompletionListener {
+            Toast.makeText(this, "animation ${it+1} completed", Toast.LENGTH_SHORT).show()
+        }
     }
 }
 
