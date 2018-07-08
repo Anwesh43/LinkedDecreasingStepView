bc69c469a264bce52322f85c17ffc8c4bb907bf8
Anwesh43 <anweshthecool0@gmail.com>
2018-07-08 11:34:41 +0530

    adding LinkedDecreasingState in Activity

--- app/src/main/java/com/anwesh/uiprojects/kotlinlinkeddecreasingstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlinlinkeddecreasingstepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.kotlinlinkeddecreasingstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.linkeddecreasingstepview.LinkedDecreasingStage
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        LinkedDecreasingStage.create(this)
     }
 }
